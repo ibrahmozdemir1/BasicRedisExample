ace12b1b7ea200c76b4e88bded7f5c1c7e720b3f
ibrahimozdemir1 <ibrahim.ozdemir@bil.omu.edu.tr>
2024-09-22 18:24:14 +0300

    Add project files.

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet("set")]
+        public async Task<IActionResult> Set(string name, string surname)
+        {
+            await _distributedCache.SetStringAsync("name", name, options: new()
+            {
+                AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(1),
+                SlidingExpiration = TimeSpan.FromSeconds(30)
+            });
+            await _distributedCache.SetAsync("surname",Encoding.UTF8.GetBytes(surname),options: new()
+            {
+                AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(1),
+                SlidingExpiration = TimeSpan.FromSeconds(30)
+            });
+
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> Get()
+        {
+
+            var name = await _distributedCache.GetStringAsync("name");
+            var surname = await _distributedCache.GetAsync("surname");
+            var surnameString = string.Empty;
+            if(surname != null)
+            {
+                surnameString = Encoding.UTF8.GetString(surname);
+            }
+            return Ok(new
+            {
+                name,
+                surnameString
+            });
+        }
+    }
+}
---
+++ Redis.Sentinel/Services/RedisService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace Redis.Sentinel.Services
+{
+    public class RedisService 
+    {
+        static ConfigurationOptions sentinelOptions => new()
+        {
+            EndPoints =
+            {
+                { "localhost", 6383 },
+                { "localhost", 6384 },
+                { "localhost", 6385 }
+            },
+            CommandMap =  CommandMap.Sentinel,
+            AbortOnConnectFail = false
+        };
+
+        static ConfigurationOptions masterOptions => new()
+        {
+            AbortOnConnectFail = false
+        };
+
+        static public async Task<IDatabase> RedisMasterDatabase()
+        {
+            ConnectionMultiplexer sentinelConnection = await 
+                ConnectionMultiplexer.SentinelConnectAsync(sentinelOptions);
+
+
+            System.Net.EndPoint masterEndpoint = null;
+            foreach(System.Net.EndPoint endpoint in sentinelConnection.GetEndPoints())
+            {
+                var server = sentinelConnection.GetServer(endpoint);
+
+                if (server.IsConnected)
+                {
+                    continue;
+                }
+
+                masterEndpoint = await server.SentinelGetMasterAddressByNameAsync("mymaster");
+                break;
+            }
+
+            var localMasterIp = masterEndpoint.ToString() switch
+            {
+                "172.17.0.2:6379" => "localhost:6379",
+                "172.18.0.3:6380" => "localhost:6380",
+                "172.18.0.4:6381" => "localhost:6381",
+                "172.18.0.5:6382" => "localhost:6382",
+            };
+
+            ConnectionMultiplexer masterConnection = await ConnectionMultiplexer.ConnectAsync(localMasterIp);
+
+            var database = masterConnection.GetDatabase();
+
+            return database;
+        }
+    }
+}
